1cb0f2c94291f6ff9e67295077259acd8c2fc43c
David Zaharee <dmzaharee@plymouth.edu>
2013-12-08 21:03:13 -0500

    add fields; show custom post on main page; fixes
    Add our custom fields to the bottom of the post content. (Todo: format
    these fields.) Add our custom post type to the main wordpress page. Use
    closures where possible to clean up unnecessary functions and globals.
    Add "joblist_" prefix so our functions and variables don't interfere
    with other plugins. Flush rewrite rules on activation. Add various todos
    where appropriate. Remove template.php as it's no longer needed.

--- ugn_plugin/main.php
+++ ugn_plugin/main.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: UGN Job Posting Plugin
  * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
@@ -10,54 +9,85 @@
  * License: GPL2 License
  */
 
-
-/*  Copyright 2013 "Unimaginative Group Name", contact via Plymouth State University
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+/**
+ * Copyright 2013 "Unimaginative Group Name", contact via Plymouth State University
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-function my_custom_post_product() {
-    $labels = array(
-        'name'               => _x( 'Job Posting', 'post type general name' ),
-        'singular_name'      => _x( 'Job Posting', 'post type singular name' ),
-        'add_new'            => _x( 'Add New', 'book' ),
-        'add_new_item'       => __( 'Post a New Job' ),
-        'edit_item'          => __( 'Edit a Job Posting' ),
-        'new_item'           => __( 'New Job Posting' ),
-        'all_items'          => __( 'All Job Postings' ),
-        'view_item'          => __( 'View Job Posting' ),
-        'search_items'       => __( 'Search Job Postings' ),
-        'not_found'          => __( 'No Matching Job Postings' ),
-        'not_found_in_trash' => __( 'No Job Postings Found In The Trash' ),
-        'parent_item_colon'  => '',
-        'menu_name'          => 'Job Posting'
-    );
-    $args = array(
-        'labels'        => $labels,
-        'description'   => 'This is the custom post type for the SoftEng class with Porter, Job Posting',
-        'public'        => true,
-        'menu_position' => 5,
-				'menu_icon'			=> plugins_url( 'icon.png', __FILE__ ),
-        'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpts', 'comments' ),
-        'has_archive'   => true,
-    );
+define('JOBLIST_POST_TYPE', 'joblisting');
 
-    register_post_type( 'job', $args );
+/**
+ * Register our custom post type
+ */
+add_action( 'init', function () {
+    joblist_register_post_type();
+});
+function joblist_register_post_type() {
+    register_post_type( JOBLIST_POST_TYPE,
+        array(
+            'labels' => array(
+                    'name'               => _x( 'Job Posting', 'post type general name' ),
+                    'singular_name'      => _x( 'Job Posting', 'post type singular name' ),
+                    'add_new'            => _x( 'Add New', 'book' ),
+                    'add_new_item'       => __( 'Post a New Job' ),
+                    'edit_item'          => __( 'Edit a Job Posting' ),
+                    'new_item'           => __( 'New Job Posting' ),
+                    'all_items'          => __( 'All Job Postings' ),
+                    'view_item'          => __( 'View Job Posting' ),
+                    'search_items'       => __( 'Search Job Postings' ),
+                    'not_found'          => __( 'No Matching Job Postings' ),
+                    'not_found_in_trash' => __( 'No Job Postings Found In The Trash' ),
+                    'parent_item_colon'  => '',
+                    'menu_name'          => 'Job Posting'
+                ),
+            'description'   => 'This is the custom post type for creating job listings.',
+            'public'        => true,
+            'menu_position' => 5,
+            'menu_icon'     => plugins_url( 'icon.png', __FILE__ ),
+            'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpts', 'comments' ),
+            'has_archive'   => true,
+        )
+    );
 }
 
-add_action( 'init', 'my_custom_post_product' );
+/**
+ * Add job postings to the main query
+ */
+add_action( 'pre_get_posts',
+    function ($query) {
+        if ( is_home() and $query->is_main_query() ) {
+            $post_types = $query->get('post_type');     # don't trample on what's already there
+            $post_types[] = JOBLIST_POST_TYPE;
+            $query->set('post_type', $post_types);
+        }
+    }
+);
 
-$meta_box['job'] = array(
+/**
+ * Flush rewrite rules on activiation so permalinks work
+ */
+register_activation_hook( __FILE__,
+    function () {
+        joblist_register_post_type();
+        flush_rewrite_rules();
+    }
+);
+
+/**
+ * Define our custom fields
+ */
+$joblist_metabox = array(
     'id' => 'jobs-meta-details',
     'title' => 'Job Details',
     'context' => 'normal',
@@ -70,7 +100,6 @@ $meta_box['job'] = array(
             'type' => 'date',
             'default' => ''
         ),
-
         array(
             'name' => 'Expiration Date:',
             'desc' => '',
@@ -78,7 +107,6 @@ $meta_box['job'] = array(
             'type' => 'date',
             'default' => ''
         ),
-
         array(
             'name' => 'Department:',
             'desc' => '',
@@ -86,7 +114,6 @@ $meta_box['job'] = array(
             'type' => 'textarea2',
             'default' => ''
         ),
-
         array(
             'name' => 'Qualifications:',
             'desc' => '',
@@ -94,7 +121,6 @@ $meta_box['job'] = array(
             'type' => 'textarea3',
             'default' => ''
         ),
-
         array(
             'name' => 'Work Study Required:',
             'desc' => '',
@@ -105,7 +131,6 @@ $meta_box['job'] = array(
                 array('name' => 'No', 'value' => 'Value 2')),
             'default' => ''
         ),
-
         array(
             'name' => 'Pay Information (Optional):',
             'desc' => '',
@@ -113,7 +138,6 @@ $meta_box['job'] = array(
             'type' => 'textarea3',
             'default' => ''
         ),
-
         array(
             'name' => 'Contact Email:',
             'desc' => '',
@@ -121,7 +145,6 @@ $meta_box['job'] = array(
             'type' => 'textarea2',
             'default' => ''
         ),
-
         array(
             'name' => 'Upload File:',
             'desc' => '',
@@ -132,8 +155,133 @@ $meta_box['job'] = array(
     )
 );
 
-add_action('admin_menu', 'plib_add_box');
+/**
+ * Add our custom fields
+ */
+add_action( 'add_meta_boxes_' . JOBLIST_POST_TYPE,
+    function () use ($joblist_metabox) {
+        add_meta_box($joblist_metabox['id'], $joblist_metabox['title'],
+            function () use ($joblist_metabox) {
+                global $post;
 
+                if ( $post->post_type !== JOBLIST_POST_TYPE ) { return; }   # if this isn't our party, leave
+
+                // Use once for verification
+                wp_nonce_field( 'joblist_metabox', 'joblist_metabox_nonce' );
+
+                echo '<table class="form-table">';
+
+                foreach ($joblist_metabox['fields'] as $field) {
+                    // get current post meta data
+                    $meta = get_post_meta($post->ID, $field['id'], true);
+
+                    echo '<tr><th style="width:20%"><label for="'. $field['id'] .'">'. $field['name']. '</label></th><td>';
+
+                    switch ($field['type']) {
+                    case 'text':
+                        echo '<input type="text" name="'. $field['id']. '" id="'. $field['id'] .'" value="'. ($meta ? $meta : $field['default']) . '" size="30" style="width:97%" />'. '<br />'. $field['desc'];
+                        break;
+                    case 'textarea':
+                        echo '<textarea name="'. $field['id']. '" id="'. $field['id']. '" cols="60" rows="4" style="width:97%">'. ($meta ? $meta : $field['default']) . '</textarea>'. '<br />'. $field['desc'];
+                        break;
+                    case 'select':
+                        echo '<select name="'. $field['id'] . '" id="'. $field['id'] . '">';
+                        foreach ($field['options'] as $option) {
+                            echo '<option '. ( $meta == $option ? ' selected="selected"' : '' ) . '>'. $option . '</option>';
+                        }
+                        echo '</select>';
+                        break;
+                    case 'radio':
+                        foreach ($field['options'] as $option) {
+                            echo '<input type="radio" name="' . $field['id'] . '" value="' . $option['value'] . '"' . ( $meta == $option['value'] ? ' checked="checked"' : '' ) . ' />' . $option['name'];
+                        }
+                        break;
+                    case 'checkbox':
+                        echo '<input type="checkbox" name="' . $field['id'] . '" id="' . $field['id'] . '"' . ( $meta ? ' checked="checked"' : '' ) . ' />';
+                        break;
+                    case 'date':
+                        echo '<input type="date" name="'. $field['id']. '" id="'. $field['id'] .'" value="'. ($meta ? $meta : $field['default']) . '" size="30" style="width:97%" />'. '<br />'. $field['desc'];
+                        break;
+                    case 'textarea2':
+                        echo '<textarea name="'. $field['id']. '" id="'. $field['id']. '" cols="60" rows="1" style="width:97%">'. ($meta ? $meta : $field['default']) . '</textarea>'. '<br />'. $field['desc'];
+                        break;
+                    case 'textarea3':
+                        echo '<textarea name="'. $field['id']. '" id="'. $field['id']. '" cols="60" rows="4" style="width:97%">'. ($meta ? $meta : $field['default']) . '</textarea>'. '<br />'. $field['desc'];
+                        break;
+                    case 'upload':
+                        echo '<label for="file">Filename:</label><input type="file" name="file" id="file"> <br> ';
+                    }
+
+                    echo '<td></tr>';
+                }
+
+                echo '</table>';
+            },
+            JOBLIST_POST_TYPE, $joblist_metabox['context'], $joblist_metabox['priority']
+        );
+    }
+);
+
+/**
+ * handle our custom fields on save
+ */
+add_action('save_post',
+    function ($post_id) use ($joblist_metabox) {
+        global $post;
+
+        if ( $post->post_type !== JOBLIST_POST_TYPE ) { return; }   # if this isn't our party, leave
+
+        //Verify nonce
+        if (!wp_verify_nonce($_POST['joblist_metabox_nonce'], 'joblist_metabox')) {
+            return $post_id;
+        }
+
+        //Check autosave
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+            return $post_id;
+        }
+
+        //Check permissions
+        if ('page' == $_POST['post_type']) {
+            if (!current_user_can('edit_page', $post_id)) {
+                return $post_id;
+            }
+        } elseif (!current_user_can('edit_post', $post_id)) {
+            return $post_id;
+        }
+
+        foreach ($joblist_metabox['fields'] as $field) {
+            $old = get_post_meta($post_id, $field['id'], true);
+            $new = sanitize_text_field( $_POST[$field['id']] );
+
+            if ($new && $new != $old) {
+                update_post_meta($post_id, $field['id'], $new);
+            } elseif ('' == $new && $old) {
+                delete_post_meta($post_id, $field['id'], $old);
+            }
+        }
+    }
+);
+
+
+/**
+ * hook into the_post and tack on our formatted extra content
+ */
+add_action('the_content',
+    function ($content) use ($joblist_metabox) {
+        global $post;
+        if ( $post->post_type !== JOBLIST_POST_TYPE ) { return; }   # if this isn't our party, leave
+
+        foreach ($joblist_metabox['fields'] as $field) {
+            $meta = get_post_meta($post->ID, $field['id'], true);
+            # todo: format custom fields here
+            $content .= "<br />{$field['name']} $meta";    # this is just filler
+        }
+        return $content;
+    }
+);
+
+# todo: What does this do? We're not using it anywhere...
 function wp_custom_attachment() {
     wp_nonce_field(plugin_basename(__FILE__), 'wp_custom_attachment_nonce');
 
@@ -145,106 +293,7 @@ function wp_custom_attachment() {
     echo $html;
 }
 
-//Add meta boxes to post types
-function plib_add_box() {
-    global $meta_box;
-
-    foreach($meta_box as $post_type => $value) {
-        add_meta_box($value['id'], $value['title'], 'plib_format_box', $post_type, $value['context'], $value['priority']);
-    }
-}
-
-//Format meta boxes
-function plib_format_box() {
-    global $meta_box, $post;
-
-    // Use once for verification
-    echo '<input type="hidden" name="plib_meta_box_nonce" value="', wp_create_nonce(basename(__FILE__)), '" />';
-
-    echo '<table class="form-table">';
-
-    foreach ($meta_box[$post->post_type]['fields'] as $field) {
-        // get current post meta data
-        $meta = get_post_meta($post->ID, $field['id'], true);
-
-        echo '<tr>'.
-            '<th style="width:20%"><label for="'. $field['id'] .'">'. $field['name']. '</label></th>'.
-            '<td>';
-        switch ($field['type']) {
-        case 'text':
-            echo '<input type="text" name="'. $field['id']. '" id="'. $field['id'] .'" value="'. ($meta ? $meta : $field['default']) . '" size="30" style="width:97%" />'. '<br />'. $field['desc'];
-            break;
-        case 'textarea':
-            echo '<textarea name="'. $field['id']. '" id="'. $field['id']. '" cols="60" rows="4" style="width:97%">'. ($meta ? $meta : $field['default']) . '</textarea>'. '<br />'. $field['desc'];
-            break;
-        case 'select':
-            echo '<select name="'. $field['id'] . '" id="'. $field['id'] . '">';
-            foreach ($field['options'] as $option) {
-                echo '<option '. ( $meta == $option ? ' selected="selected"' : '' ) . '>'. $option . '</option>';
-            }
-            echo '</select>';
-            break;
-        case 'radio':
-            foreach ($field['options'] as $option) {
-                echo '<input type="radio" name="' . $field['id'] . '" value="' . $option['value'] . '"' . ( $meta == $option['value'] ? ' checked="checked"' : '' ) . ' />' . $option['name'];
-            }
-            break;
-        case 'checkbox':
-            echo '<input type="checkbox" name="' . $field['id'] . '" id="' . $field['id'] . '"' . ( $meta ? ' checked="checked"' : '' ) . ' />';
-            break;
-        case 'date':
-            echo '<input type="date" name="'. $field['id']. '" id="'. $field['id'] .'" value="'. ($meta ? $meta : $field['default']) . '" size="30" style="width:97%" />'. '<br />'. $field['desc'];
-            break;
-        case 'textarea2':
-            echo '<textarea name="'. $field['id']. '" id="'. $field['id']. '" cols="60" rows="1" style="width:97%">'. ($meta ? $meta : $field['default']) . '</textarea>'. '<br />'. $field['desc'];
-            break;
-        case 'textarea3':
-            echo '<textarea name="'. $field['id']. '" id="'. $field['id']. '" cols="60" rows="4" style="width:97%">'. ($meta ? $meta : $field['default']) . '</textarea>'. '<br />'. $field['desc'];
-            break;
-        case 'upload':
-            echo '<label for="file">Filename:</label><input type="file" name="file" id="file"> <br> ';
-        }
-        echo     '<td>'.'</tr>';
-    }
-    echo '</table>';
-}
-
-// Save data from meta box
-function plib_save_data($post_id) {
-    global $meta_box,  $post;
-
-    //Verify nonce
-    if (!wp_verify_nonce($_POST['plib_meta_box_nonce'], basename(__FILE__))) {
-        return $post_id;
-    }
-
-    //Check autosave
-    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
-        return $post_id;
-    }
-
-    //Check permissions
-    if ('page' == $_POST['post_type']) {
-        if (!current_user_can('edit_page', $post_id)) {
-            return $post_id;
-        }
-    } elseif (!current_user_can('edit_post', $post_id)) {
-        return $post_id;
-    }
-
-    foreach ($meta_box[$post->post_type]['fields'] as $field) {
-        $old = get_post_meta($post_id, $field['id'], true);
-        $new = $_POST[$field['id']];
-
-        if ($new && $new != $old) {
-            update_post_meta($post_id, $field['id'], $new);
-        } elseif ('' == $new && $old) {
-            delete_post_meta($post_id, $field['id'], $old);
-        }
-    }
-}
 /*
-add_action('save_post', 'plib_save_data');
 function job_page(){
 	echo ("hello world");
 	 $my_post = array(
@@ -270,5 +319,3 @@ if ( have_posts() ) {
 		} // end while
 	} // end if
 */
-
-?>
